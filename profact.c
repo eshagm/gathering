5f1d7fa20e26b3dd9a76058588a156497df86b77
eshagm <131762914+eshagm@users.noreply.github.com>
2023-06-01 16:06:04 +0530

    gathering

---
+++ profact.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+fact(int n)
+{
+	int factorial;
+	if(n<=0)
+	{
+		return 1;
+	}
+	else
+	{
+		factorial =n*fact(n-1);
+	}
+	return factorial;	
+}
+main()
+{
+	int a,ans;
+	printf("enter ualue A =");
+	scanf("%d",&a);
+	ans=fact(a);
+	printf("factorial is =%d",ans);
+}
+
+
+
